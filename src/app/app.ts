10a172fbd9b3012f5d254765365706aaa1fa6ee6
Tchitos <amine.tchitooss@gmail.com>
2026-04-06 23:43:31 +0200

    test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CreditJsonEditorComponent } from "./credit/components/credit-json-editor/credit-json-editor.component";
 import { FormsModule } from "@angular/forms";
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CreditJsonEditorComponent, FormsModule],
+  imports: [CreditJsonEditorComponent, FormsModule, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
